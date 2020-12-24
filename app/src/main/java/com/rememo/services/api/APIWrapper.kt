288a15d9fc6684645e6f374a440a3c9899a23a1f
Nicola Toscan <nicolatoscan99@outlook.it>
2020-12-24 15:38:08 +0100

    fix import

--- app/src/main/java/com/rememo/services/api/APIWrapper.kt
+++ app/src/main/java/com/rememo/services/api/APIWrapper.kt
@@ -1,14 +1,8 @@
 package com.rememo.services.api
 
-import android.util.Log
 import com.github.kittinunf.fuel.Fuel
-import com.github.kittinunf.fuel.core.Request
 import com.github.kittinunf.fuel.coroutines.awaitStringResponseResult
-import com.github.kittinunf.fuel.json.FuelJson
-import com.github.kittinunf.fuel.json.responseJson
 import com.google.gson.Gson
-import com.rememo.models.Pippo
-import kotlinx.serialization.decodeFromString
 
 object APIWrapper {
     suspend inline fun <reified T> apiGet(url: String, onResult: (T) -> Unit, onError: (String) -> Unit) {
